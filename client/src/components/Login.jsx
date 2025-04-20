6f4c0deb31efa2bba354e72c6fdf52980c08d09c
FOKAM POKA ARSENE <fopoar@gmail.com>
2025-04-20 04:38:21 -0400

    student table created

--- client/src/components/Login.jsx
+++ client/src/components/Login.jsx
@@ -1,20 +1,37 @@
-import React from 'react'
+import React, { useState } from 'react'
+import axios from 'axios'
 import './style.css'
 const Login = () => {
+    const [values, setValues] = useState({
+        email : '',
+        password : ''
+    });
+
+    const handleSubmit =(event) => {
+        event.preventDefault();
+        axios.post('http://localhost:3000/auth/adminlogin')
+        .then(result => console.log(result))
+        .catch(err => console.log(err))
+    }
   return (
     <div className='d-flex justify-content-center align-items-center vh-100 loginPage'>
       <div className='p-3 rounded w-25 border loginForm '>
-        <h2>Login page</h2>
-        <form action="" >
-            <div>
+        <h2>Login </h2>
+        <form onSubmit={handleSubmit} >
+            <div className='mb-3'>
                 <label htmlFor="email"><strong>Email</strong></label>
-                <input type="email" name='email' autoCapitalize='off' placeholder='Ton email' className='form-control rounded-0' />
+                <input type="email" onChange={(e)=> setValues({...values, email:e.target.value})} name='email' autoCapitalize='off' placeholder='Ton email' className='form-control rounded-0' />
             </div>
-            <div>
+            <div className='mb-3'>
                 <label htmlFor="password"><strong>Mot de passe</strong></label>
-                <input type="password" name='password' autoCapitalize='off' placeholder='Ton mot de passe' className='form-control rounded-0' />
+                <input type="password"  onChange={(e)=> setValues({...values, password:e.target.value})} name='password' autoCapitalize='off' placeholder='Ton mot de passe' className='form-control rounded-0' />
             </div>
-            <button className="btn btn-success w-100 rounded-0"> Entrer</button>
+            <button className="btn btn-success w-100 rounded-0 mb-2"> Entrer</button>
+            <div className='mb-1'>
+                <input type="checkbox" name='tick' id='tick' className='me-2'  />
+
+                <label htmlFor="tick">Tu es d'accord avec les conditions d'utilisation</label>
+            </div>
         </form>
       </div>
     </div>
---
+++ primary.sql
@@ -0,0 +1,28 @@
+DROP DATABASE IF EXISTS primarydb;
+CREATE DATABASE primarydb ;
+use primarydb;
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+CREATE TABLE `students` (
+  `id` int(11) NOT NULL,
+  `first_name` varchar(50) DEFAULT NULL,
+  `last_name` varchar(50) DEFAULT NULL,
+  `gender` varchar(50) DEFAULT NULL,
+  `email` varchar(50) DEFAULT NULL,
+  `profile_image` varchar(50) DEFAULT NULL,
+  `matricule` varchar(50) DEFAULT NULL,
+  `country` varchar(50) DEFAULT NULL,
+  `city` varchar(50) DEFAULT NULL,
+  `street` varchar(50) DEFAULT NULL,
+  `address` varchar(50) DEFAULT NULL,
+  `birth_date` varchar(50) DEFAULT NULL,
+  `birth_place` varchar(50) DEFAULT NULL,
+  `birth_country` varchar(50) DEFAULT NULL,
+  `previous_school` varchar(50) DEFAULT NULL,
+  `region_origin` varchar(50) DEFAULT NULL,
+  `birth_certificate` varchar(50) DEFAULT NULL,
+  `created_at` datetime DEFAULT current_timestamp()
+
+)
